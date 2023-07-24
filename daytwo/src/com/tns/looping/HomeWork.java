76f01e98f08f8772d859a8c341a503c96051493e
PRANAV <PRANAV@DESKTOP-LKCDJUM>
2023-07-24 20:24:44 +0530

    First Commit

---
+++ daytwo/src/com/tns/looping/HomeWork.java
@@ -0,0 +1,31 @@
+package com.tns.looping;
+
+import java.util.Scanner;
+
+public class HomeWork {
+
+	public static void main(String[] args) {
+		System.out.println("\n----------------------------------------------\n");	
+		//For loop problem
+		
+		for(int i=1; i<=5; i++) {
+			for(int j=1; j<=i; j++) {
+				System.out.print(j+" ");
+			}
+			System.out.println();
+		}
+		
+	System.out.println("\n----------------------------------------------\n");	
+		//Do while loop Problem
+		Scanner s= new Scanner(System.in);
+		System.out.println("Enter a value of n: ");
+		int n=s.nextInt();
+		
+		do {
+			System.out.println(n);
+			n--;
+		}while(n>=1);
+
+	}
+
+}
