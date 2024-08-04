718d6082a6ef63b3637766be64ba98d000565767
Pranav Gadakh. <118759094+Pranavgadakh0108@users.noreply.github.com>
2024-08-04 18:15:47 +0530

    first commit

---
+++ Servlet_Practice/WebXmlDemo/src/main/java/in/sp/backend/Register.java
@@ -0,0 +1,17 @@
+package in.sp.backend;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class Register extends HttpServlet{
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("I am in register Service method....");
+	}
+
+}
